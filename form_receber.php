85ae0cefd2eeb09bd6ccbabe2ae2bd3bcdc20322
jairoaw <omesmojairo@gmail.com>
2023-03-14 20:46:12 -0300

    add pdo

--- form_receber.php
+++ form_receber.php
@@ -1,17 +1,39 @@
 <?php
-    $senha_cripto = '$2y$05$lEVqUvkioSsykC4sxvqxleHg0teFQzLpCSF4lKFqyRAuGjjCJguoe';
+    require('pdo.inc.php');
 
+    #$senha_cripto = '$2y$05$lEVqUvkioSsykC4sxvqxleHg0teFQzLpCSF4lKFqyRAuGjjCJguoe';
+    // Exemplo de prática não tão boa
+    // if (!isset($_POST['usuario']) || 
+    // !isset($_POST['senha'])) {
+    //     echo 'Envie o form';
+    //     die;
+    // }
+
+    // Nullish coalescing operator
     $usuario = $_POST['usuario'] ?? false;
     $senha = $_POST['senha'] ?? false;
+
+    //prepara a consulta
+    $sql = $pdo -> prepare('SELECT * FROM usuarios WHERE username = ? AND active=1');
+    $sql -> bindParam(1, $usuario, PDO::PARAM_STR);  #evita ataques de sql injection
     
-    if ($usuario=='jairo' && password_verify($senha, $senha_cripto)) {
-        //autenticação OK
+    //roda a consulta no banco
+    $sql-> execute();
+    
+    //busca os dados do banco
+    $dados = $sql -> fetch(PDO::FETCH_ASSOC);
+
+    if (($sql->rowCount() == 1) 
+        && password_verify($senha, $dados['password'])) {
+        // Autenticação OK
         session_start();
         $_SESSION['usuario'] = $usuario;
-        header ('location:boasvindas.php');
-        die; //die encerra o script
-    }else{
-        //autenticação com erro
-        header ('location:form.php?erro=1');
+        $_SESSION['admin'] = $dados['admin'];
+
+        header('location:boasvindas.php');
+        die;
+    } else {
+        // Autenticação falhou
+        header('location:form.php?erro=1');
         die;
     }
---
+++ pdo.inc.php
@@ -0,0 +1,4 @@
+<?php
+    #usar em todos arquivos com PDO (PHP Data Object)
+    #pdo.inc.php
+    $pdo = new PDO('mysql:host=localhost;dbname=exemplodsi','root',''); #sem senha
